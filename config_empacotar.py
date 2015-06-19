c42e0666abc82aef94305621e7c8cca14a035364
Andrey Santanna <andrey.santanna@ncr.com>
2015-06-19 18:19:50 -0300

    Defaults de pasta

--- config_empacotar.py
+++ config_empacotar.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 # caminho base dos seus plugins em python
-CAMINHO_PLUGINS_DEV = r'd:\Projetos\colibri-plugins-sdk\examples'
+CAMINHO_PLUGINS_DEV = r'.\examples'
 # caminho da pasta "bin\plugins" no colibri
-CAMINHO_PLUGINS_DEST = r'd:\Projetos\colibri-8\bin\plugins'
+CAMINHO_PLUGINS_DEST = r'c:\Colibri 8\colibri-8\bin\plugins'
 
